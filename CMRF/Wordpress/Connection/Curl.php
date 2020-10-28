38362223903d4f426609e6d3c7e36903bdde20f4
root <root@ovh3.jmaconsulting.biz>
2020-10-28 00:56:57 -0400

    Modifications for YHV application form

--- CMRF/Wordpress/Connection/Curl.php
+++ CMRF/Wordpress/Connection/Curl.php
@@ -26,10 +26,42 @@ class Curl extends AbstractCurl {
 
     $request               = $this->getAPI3Params($call);
     if ($call->getAction() == "volunteer_application") {
+      $yesno = [
+        'type_in_english',
+        'type_in_chinese',
+        'employee_of_yeehong',
+        'read_and_write_chinese',
+        'criminal_offence',
+        'driver',
+        'interest_class_instructor',
+        'breakfast_booth',
+        'assistant_with_meals',
+        'friendly_visiting',
+        'office_and_clerical_duties',
+        'speak_cantonese',
+        'escort_shopping',
+        'special_accomodation',
+        'speak_english',
+        'pa_nursing_home',
+        'phone_chat',
+        'kitchen_assistant',
+        'car',
+        'age_18_',
+        'speak_mandarin',
+        'reception_desk_assistant',
+        'meal_delivery',
+        'laundry_assistant',
+        'pa_community_program',
+        'i_consent',
+        'read_and_write_english'
+      ];
       foreach ($request as $key => $value) {
-    	if (is_array($value) && empty($value[0])) {
-    	  unset($request[$key]);
-	}
+        if (is_array($value) && empty($value[0])) {
+          unset($request[$key]);
+        }
+        if (in_array($key, $yesno) && empty($value)) {
+          $request[$key] = "0";
+        }
       }
     }
     // $request['api_key']    = $profile['api_key'];
@@ -40,7 +72,8 @@ class Curl extends AbstractCurl {
     $post_data = "entity=" . $call->getEntity();
     $post_data .= "&action=" . $call->getAction();
     $post_data .= "&api_key={$profile['api_key']}&key={$profile['site_key']}&version=3&XDEBUG_SESSION_START=sdf";
-    $post_data .= "&json=" . stripslashes(json_encode($request));
+    //$post_data .= "&json=" . json_encode($request);
+    $post_data .= "&json=" . urlencode(json_encode($request));
 
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_POST,           1);
